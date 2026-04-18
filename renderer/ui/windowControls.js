ee5540499be779f32818c788ac2657f80d873b00
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-04-18 14:09:35 +0200

    fix: WindowControls capitalisation (2/2)

---
+++ renderer/ui/windowControls.js
@@ -0,0 +1,27 @@
+import { isPlatformMacOS, isPlatformWeb } from '@core/Platform.js';
+
+// Handle custom titlebar interactions
+export function initWindowControls() {
+  if (isPlatformMacOS() || isPlatformWeb() || !window.electronAPI)
+    return;
+
+  document.querySelectorAll('[data-win-bar]').forEach(btn => {
+    btn.addEventListener('dblclick', (event) => {
+      if (event.target === event.currentTarget) {
+        window.electronAPI.maximize();
+      }
+    });
+  });
+
+  document.querySelectorAll('[data-win-min]').forEach(btn => {
+    btn.addEventListener('click', () => window.electronAPI.minimize());
+  });
+
+  document.querySelectorAll('[data-win-max]').forEach(btn => {
+    btn.addEventListener('click', () => window.electronAPI.maximize());
+  });
+
+  document.querySelectorAll('[data-win-close]').forEach(btn => {
+    btn.addEventListener('click', () => window.electronAPI.close());
+  });
+}
