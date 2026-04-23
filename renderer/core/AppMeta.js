505c68362e14177edb704d6afc05bc0d6e165b53
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-04-23 10:30:37 +0200

    feat: version 1.1

--- renderer/core/AppMeta.js
+++ renderer/core/AppMeta.js
@@ -5,7 +5,7 @@ import { parseMarkdown } from '@common/MarkdownParser.js';
 // Single source of truth for the application meta data.
 
 export const APP_NAME = 'DocForge';
-export const APP_VERSION = '1.0';
+export const APP_VERSION = '1.1';
 
 export const FILE_EXTENSION_PROJECT = '.dfproj';
 export const FILE_EXTENSION_DOC_THEME = '.dftheme';
