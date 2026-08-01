4c175d5adf72ff0daad2b0cc5cdca9445779877d
Etienne780 <162643852+Etienne780@users.noreply.github.com>
2026-08-01 16:02:32 +0200

    fix: removed test editor view

--- renderer/views/languageEditor/LanguageEditorView.js
+++ renderer/views/languageEditor/LanguageEditorView.js
@@ -22,39 +22,11 @@ export class LanguageEditorView extends BaseView {
     ]);
 
     this._instanceIds = instances.map(i => i.instanceId); */
-
-    const HTMLInput = document.getElementById('language_input');
-    const HTMLContainer = document.getElementById('language_output');
-    addTabIndenting(HTMLInput);
-    this._resize = new ResizeController(document.getElementById('language_input_wrapper'), { 
-      keepRatio: false,
-      direction: 'right',
-    });
-
-    const def = findSyntaxDefinitionByName('Assembly');
-
-    // this._removeHighlighter = syntaxHighlighter.autoHighlightTextById(
-    //   {
-    //     langId: def.id,
-    //     styleId: null,
-    //     inputHTML: HTMLInput,
-    //     outputHTML: HTMLContainer,
-    //     debounceTimeMS: 300,
-    //   }
-    // );
-
-    this._removeHighlighter = syntaxHighlighter.highlightExampleToElement({
-      outputElement: HTMLContainer, 
-      alias: 'Assembly', 
-      styleId: null,
-    });
-
     shortcutManager.setContext('languageEditor');
   }
 
   onDestroy() {
     this._resize.destroy();
-    this._removeHighlighter?.();
   }
 
 }
